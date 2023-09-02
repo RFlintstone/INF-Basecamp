5a2974553b2c206f246547512729f32d7cdcae97
RFlintstone <rubenflint@hotmail.com>
2023-09-02 15:59:52 +0200

    initial commit

---
+++ A1W1A1 - Year to month & day/main.py
@@ -0,0 +1,20 @@
+# Implement a program that given number of years as input prints the number of months and days as output.
+#
+# Criteria:
+# keep the problem simple
+# no leap year
+# each year is 365 days and 12 months
+# Input example:
+# Years: 5
+#
+# Output example:
+# Months: 60, Days: 1825
+
+print('Enter amount of years:')
+y = int(input())
+m = y * 12
+d = y * 365
+
+print('Years: ' + str(y))
+print('Months: ' + str(m))
+print('Days: ' + str(d))
---
+++ A1W1A2 - Tax & Tip/main.py
@@ -0,0 +1,25 @@
+# The program that you create for this exercise will begin by reading the cost of a meal ordered at a restaurant from the user.
+# Then your program will compute the tax and tip for the meal. Use your local tax rate when computing the amount of tax owing.
+# The output from your program should include the tax amount, the tip amount, and the grand total for the meal including both the tax and the tip.
+#
+# Criteria:
+# Tip is 15 percent of meal amount (without the tax)
+# Assume a local tax rate of 21 percent
+# Round all numbers up to 3 decimals in the output
+#
+# Input example:
+# Cost of the meal: 23.60
+#
+# Output example:
+# Tax: 4.956 , Tip: 3.540 , Total: 32.096
+
+print('Enter costs of the meal:')
+price = float(input())
+tip = price / 100 * 15
+tax = price / 100 * 21
+total = price + tip + tax
+
+print('Meal price: %.3f' % price)
+print('Meal tip: %.3f' % tip)
+print('Meal tax: %.3f' % tax)
+print('Meal total: %.3f' % total)
