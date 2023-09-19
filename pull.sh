27b03556b2dc9a7d40cfc7505672bad74bebfc2a
RFlintstone <rubenflint@hotmail.com>
2023-09-19 14:31:25 +0200

    Added pull script (.sh)

--- pull.sh
+++ pull.sh
@@ -1,1 +1,13 @@
-git reset --hard && git pull
+read -p "The script will do a hard reset and pull the latest files. Are you sure? (y/n)?" choice
+case "$choice" in
+  y|Y )
+  git reset --hard
+  git pull
+  ;;
+  n|N )
+  echo "Canceling operation"
+  ;;
+  * )
+  echo "Invalid choice. Please run the script again."
+  ;;
+esac
