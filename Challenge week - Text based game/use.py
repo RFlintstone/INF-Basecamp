1af86c2c0405905fb867a701c0ee214e97a13eab
RFlintstone <rubenflint@hotmail.com>
2023-09-25 10:34:18 +0200

    Added debug mode to the file use.py

--- Challenge week - Text based game/use.py
+++ Challenge week - Text based game/use.py
@@ -1,3 +1,6 @@
+# Debugging True or False
+DEBUG_MODE = False
+
 # Create our inventory
 inv = list()
 
@@ -5,6 +8,7 @@ inv = list()
 inv.append("wrench")
 
 
+# Create a method so we can execute 'commands'
 def command(cmd_arg, action_arg):
     # Lower the command and action
     cmd_arg = cmd_arg.lower()
@@ -13,15 +17,17 @@ def command(cmd_arg, action_arg):
     # If we are going to use an item
     if cmd_arg == "use":
         # Check if we have the item
-        if action_arg.lower() in inv:
-            # Print the inventory
-            print(inv)
+        if action_arg in inv:
+            # Print the inventory if debug mode is on
+            if DEBUG_MODE:
+                print(inv)
             # Print that we are using the item
             print("Using item: " + action_arg)
             # Remove the item from our inventory
             inv.remove(action_arg)
-            # Print our (updated) inventory
-            print(inv)
+            # Print our (updated) inventory if debug mode is on
+            if DEBUG_MODE:
+                print(inv)
         # If we don't have the item we are trying to use
         else:
             # Print that we don't have the item
