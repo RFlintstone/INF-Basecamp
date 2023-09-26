65d53d46d51b490a474a75249ea34febed093769
RFlintstone <rubenflint@hotmail.com>
2023-09-26 10:04:22 +0200

    Added testroom

--- Challenge week - Text based game/testroom.py
+++ Challenge week - Text based game/testroom.py
@@ -1,73 +1,67 @@
+import json
 import commands as c
-
-rooms = {
-    'Start': {
-        'description': 'You are in a small room with a wooden chest in the corner.',
-        'exits': {'east': 'Hallway'},
-        'items': ['small key'],
-        'npcs': ['Bob']
-    },
-
-    'Hallway': {
-        'description': 'You are in a long hallway.',
-        'exits': {'west': 'Start', 'north': 'Dungeon'},
-        'items': [],
-        'npcs': []
-    },
-
-    'Dungeon': {
-        'description': 'You are in a dark dungeon.',
-        'exits': {},
-        'items': ['torch'],
-        'npcs': []
-    }
-}
 
 # Add item to library
 c.add_inv("small key")
 
-stop_loop = False
 
+def setup_room(my_room_name):
+    with open('rooms.json') as f:
+        rooms = json.load(f)
 
-def hallway1():
     # Set starting room
-    current_room = 'Hallway'
+    current_room = my_room_name.capitalize()
 
     # Print current room description
     print(rooms[current_room]['description'])
 
+    return rooms, current_room
+
+
+def hallway1(rooms, current_room):
+    stop_loop = False
+
     i = input("What do you want to do?").split(" ")
     print(i)
+    if len(i) < 2:
+        print("You didn't specify an action")
+        stop_loop = False
+        return stop_loop
+    else:
+        # Call GO c.command
+        go = c.command(i[0], i[1])
 
-    # Call GO c.command
-    go = c.command(i[0], i[1])
+        if i[0].lower() == "go" and go == ("N" or "E" or "S" or "W"):
+            current_room = rooms[current_room]['exits']['north']
+            print(rooms[current_room]['description'])
+            stop_loop = True
 
-    if i[0].lower() == "go" and go == ("N" or "E" or "S" or "W"):
-        current_room = rooms[current_room]['exits']['north']
-        print(rooms[current_room]['description'])
-        stop_loop = True
+        # Call INSPECT c.command on chest
+        if i[0].lower() == "inspect":
+            inspect = c.command("inspect", "chest")
+            print("INSPECT: " + str(inspect))
 
-    # Call INSPECT c.command on chest
-    if i[0].lower() == "inspect":
-        inspect = c.command("inspect", "chest")
-        print("INSPECT: " + str(inspect))
+        # Call USE c.command on key
+        if i[0].lower() == "use":
+            use = c.command("use", "small key")
+            print("USE: " + str(use))
 
-    # Call USE c.command on key
-    if i[0].lower() == "use":
-        use = c.command("use", "small key")
-        print("USE: " + str(use))
+            if i[0].lower() == "use" and use is True:
+                print("Unlocked chest.")
 
-    if i[0].lower() == "use" and use is True:
-        print("Unlocked chest.")
+        # Call TALK c.command
+        if i[0].lower() == "talk":
+            talk = c.command("talk", "bob")
+            print("TALK: " + str(talk))
 
-    # Call TALK c.command
-    if i[0].lower() == "talk":
-        talk = c.command("talk", "bob")
-        print("TALK: " + str(talk))
+            if i[0].lower() == "talk" and talk is True:
+                print("Dialog with Bob")
 
-    if i[0].lower() == "talk" and talk is True:
-        print("Dialog with Bob")
+        return stop_loop
 
 
-while stop_loop is False:
-    hallway1()
+stop_loop_ = False
+room_json, room_name = setup_room("Hallway")
+
+while stop_loop_ is False:
+    stop_loop_ = hallway1(room_json, room_name)
