b69c20d77620a11693a7a335ed3a1c11803983ac
RFlintstone <rubenflint@hotmail.com>
2024-01-11 18:12:55 +0100

    Better SQL handling almost done, stuck on lastrowid not properly updating

--- A4W16M1 - Final Project - Master Assignment/iceskatingapp.py
+++ A4W16M1 - Final Project - Master Assignment/iceskatingapp.py
@@ -10,10 +10,13 @@ from event import Event
 
 
 def main():
+    # Prepare the script (such as resetting the database)
     prepare()
 
+    # Create a database object
     db = Database()
 
+    # Our test skater
     s = Skater(
         id=-1,
         first_name='Bob',
@@ -23,9 +26,19 @@ def main():
         date_of_birth='01-01-2000',
         database=db
     )
-    s.insert_skater()
 
-    # s.print_all_attributes_but_fancy("Overview")
+    # Prepare an insert query which we can execute later.
+    db.db_insert_prep("skaters", "first_name, last_name, nationality, gender, date_of_birth")
+
+    # Execute the prepared query with own values, so we can insert values into our database
+    db.execute_query([s.first_name, s.last_name, s.nationality, s.gender, s.date_of_birth])
+    db.execute_query([s.first_name, s.last_name, s.nationality, s.gender, s.date_of_birth])
+
+    # Fetch row from table based on specific id
+    db.db_fetch("skaters", "id", "1")
+
+    # Fetch all rows of a specific table
+    db.db_fetch("skaters")
 
     # t = Track(
     #     id=1,
@@ -35,9 +48,7 @@ def main():
     #     outdoor=True,
     #     altitude=1000
     # )
-    #
-    # t.print_all_attributes_but_fancy("Overview")
-    #
+
     e = Event(
         id=1,
         name='Sample Event',
@@ -50,9 +61,9 @@ def main():
         category='Human'
     )
 
-    # e.print_all_attributes_but_fancy("Overview")
-
-    e.add_skater(s)
+    db.db_insert_prep("event_skaters", "event_id, skater_id")
+    db.execute_query([e.id, s.id])
+    db.db_fetch("event_skaters")
 
 
 def prepare():
@@ -67,20 +78,81 @@ def prepare():
 
 
 class Database:
-    def db_connect(self):
-        db_file = os.path.join(sys.path[0], 'iceskatingapp.db')
+    def __init__(self):
+        self.db_file = os.path.join(sys.path[0], 'iceskatingapp.db')
+        self.conn = self.db_connect()
+        self.query = ""
+        self.lastrowid = -1
 
-        if not os.path.exists(db_file):
-            print(f"Error - Database file {db_file} does not exist")
+    def db_connect(self):
+        if not os.path.exists(self.db_file):
+            print(f"Error - Database file {self.db_file} does not exist")
             return
 
-        conn = sqlite3.connect(db_file)
+        conn = sqlite3.connect(self.db_file)
 
         if conn is None:
-            print(f"Error - Failed to connect to {os.path.basename(db_file)}")
+            print(f"Error - Failed to connect to {os.path.basename(self.db_file)}")
         else:
-            print(f"Successfully connected to: {os.path.basename(db_file)}")
+            print(f"Successfully connected to: {os.path.basename(self.db_file)}")
+
         return conn
+
+    def db_insert_prep(self, table, columns):
+        num_ques = len(columns.split(", "))
+        value_promise = ("?," * (num_ques - 1)) + "?"
+        self.query = f"INSERT INTO {table} ({columns}) VALUES ({value_promise})"
+
+        print(self.query)
+
+    def db_fetch(self, table, where=None, id=None, silent=False):
+        c = self.conn.cursor()
+
+        self.query = f"SELECT * FROM {table}"
+
+        if where and id is not None:
+            self.query += f" WHERE {where}=?"
+
+        if id is not None:
+            values = (id,)
+        else:
+            values = ()
+
+        c.execute(self.query, values)
+
+        self.lastrowid = c.lastrowid
+
+        if not silent:
+            print(self.query)
+
+            # Fetch column names and print them joined by ' | '
+            names = [description[0] for description in c.description]
+            header = ' | '.join(f"{name:<20}" for name in names)
+            print("\n")
+
+            # Print the column names
+            print(header)
+
+            # Print a dashed line of the same width as the header
+            print('-' * len(header))
+
+            # Fetch and print all rows
+            rows = c.fetchall()
+            if len(rows) == 0:
+                print("No rows")
+            else:
+                for row in rows:
+                    print(' | '.join(
+                        f"{str(r) if r is not None else str(type(r)):<20}" for r in row))  # Format items and concatenate
+                print("\n")
+        c.close()
+
+    def execute_query(self, values: list):
+        c = self.conn.cursor()
+        c.execute(self.query, values)
+        self.lastrowid = c.lastrowid
+        c.connection.commit()
+        c.close()
 
 
 if __name__ == "__main__":
--- A4W16M1 - Final Project - Master Assignment/skater.py
+++ A4W16M1 - Final Project - Master Assignment/skater.py
@@ -8,14 +8,15 @@ from datetime import datetime
 class Skater:
     def __init__(self, id: int, first_name: str, last_name: str, nationality: str,
                  gender: str, date_of_birth: datetime.date, database) -> None:
-        self.id = id
+        self.database = database
+        self.database.db_fetch(table="skaters", silent=True)
+
+        self.id = self.database.lastrowid
         self.first_name = first_name
         self.last_name = last_name
         self.nationality = nationality
         self.gender = gender
         self.date_of_birth = date_of_birth
-        self.database = database
-        self.db_conn = self.database.db_connect()
 
     def __repr__(self) -> str:
         return "{}({})".format(type(self).__name__,
@@ -28,46 +29,6 @@ class Skater:
 
     def get_events(self) -> list:
         return self.__repr__()
-
-    def insert_skater(self):
-        # Create a database connection
-        insert_sql = '''INSERT INTO skaters (first_name, last_name, nationality, gender, date_of_birth) VALUES (?, ?, ?, ?, ?)'''
-        c = self.db_conn.cursor()
-
-        c.execute(insert_sql, (self.first_name, self.last_name, self.nationality, self.gender, self.date_of_birth))
-        self.id = c.lastrowid
-
-        c.connection.commit()
-        c.close()
-
-        self.fetch_scater()
-
-    def fetch_scater(self):
-        # Create a database connection
-        c = self.db_conn.cursor()
-        c.execute("SELECT * FROM skaters")
-
-        # Fetch column names and print them joined by ' | '
-        names = [description[0] for description in c.description]
-        header = ' | '.join(f"{name:<20}" for name in names)
-        print("\n")
-
-        # Print the column names
-        print(header)
-
-        # Print a dashed line of the same width as the header
-        print('-' * len(header))
-
-        # Fetch and print all rows
-        rows = c.fetchall()
-        if len(rows) == 0:
-            print("No rows")
-        else:
-            for row in rows:
-                print(' | '.join(
-                    f"{str(r) if r is not None else str(type(r)):<20}" for r in row))  # Format items and concatenate
-            print("\n")
-        c.close()
 
     def print_all_attributes_but_fancy(self, task_name: str = "") -> None:
         name = type(self).__name__
